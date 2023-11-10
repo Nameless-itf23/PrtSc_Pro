96b9b5e4037216857663289620174582324d1031
Nameless-itf23 <ohutoncat@gmail.com>
2023-11-11 00:16:19 +0900

    handle error

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ import save
 
 def on_key_event(e):
     if keyboard.is_pressed('ctrl+alt+p'):
-        save_func, name, now = screenshot.screenshot()
-        save.save(save_func, name, now)
-        brightness = sbc.get_brightness()[0]
-        sbc.set_brightness(0)
-        time.sleep(0.3)
-        sbc.set_brightness(brightness)
+        try:
+            save_func, name, now = screenshot.screenshot()
+            save.save(save_func, name, now)
+            brightness = sbc.get_brightness()[0]
+            sbc.set_brightness(0)
+            time.sleep(0.3)
+            sbc.set_brightness(brightness)
+        except:
+            print('an error occurred')
 
 
 if __name__ == "__main__":
